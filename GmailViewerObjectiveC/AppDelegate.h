5b8c4f195f218c87186851ac590f9b4b4f79ba68
UQ Times <uqtimes@gmail.com>
2014-10-14 22:25:37 +0900

    Initial Commit

---
+++ GmailViewerObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GmailViewerObjectiveC
+//
+//  Created by Ryo Tsuruda on 10/14/14.
+//  Copyright (c) 2014 UQ Times. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GmailViewerObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GmailViewerObjectiveC
+//
+//  Created by Ryo Tsuruda on 10/14/14.
+//  Copyright (c) 2014 UQ Times. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
